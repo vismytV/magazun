04d728a2bf99d186e711dedd0c60901a90d84ee0
vismyt <vismyt2016@gmail.com>
2024-12-21 18:46:08 +0200

    майже нічого нового

--- magazun/Controllers/ProductController.cs
+++ magazun/Controllers/ProductController.cs
@@ -57,14 +57,18 @@ namespace magazun.Controllers
 
 		//історія юзера
 		[HttpPost]
-		public IActionResult Histori(string lastName1, string firstName1, string role1)
+		public IActionResult Histori(string lastName1, string firstName1, string role1,string login_user1)
 		{
 			TempData["lastName"] = lastName1;
 			TempData["firstName"] = firstName1;
 			TempData["error1"] = "";
 			TempData["role"] = role1;
+			TempData["login_user"] = login_user1;
 
-			var customer=_database.GetCustomer().FirstOrDefault(c=>c.LastName==lastName1 && firstName1==firstName1);
+			var login=_database.GetLogin().FirstOrDefault(l=>l.UserLogin== login_user1);
+
+			var customer=_database.GetCustomer()
+				.FirstOrDefault(c=>c.CustomerId==login.idCustomer);
 
 			// Отримуємо всі ордери для знайденого користувача
 			var orders = _database.GetOrder()
@@ -73,9 +77,9 @@ namespace magazun.Controllers
 				.Where(z => z.CustomerId == customer.CustomerId) // Фільтруємо за CustomerId
 				.ToList();
 
-			int a;
+			//int a;
 			ViewBag.Orders = orders;
-				a=orders.Count();
+				//a=orders.Count();
 
 			var p = _database.GetOrder().FirstOrDefault(o => o.CustomerId == customer.CustomerId);
 			if (p != null)
@@ -85,10 +89,17 @@ namespace magazun.Controllers
 			return View();
 		}
 
-		//додаэмо новий ордер
+		//додаэмо новий ордер+++++++++++++++++++
 		[HttpPost]
 		public IActionResult AddOrder(int customerId, List<int> productIds)
 		{
+			/*TempData["lastName"] = lastName1;
+			TempData["firstName"] = firstName1;
+			TempData["error1"] = "";
+			TempData["role"] = role1;
+			TempData["login_user"] = login_user1;*/
+
+
 			if (productIds == null || !productIds.Any())
 				return BadRequest("No products specified for the order.");
 
@@ -112,42 +123,17 @@ namespace magazun.Controllers
 			}
 		}
 
-		//[HttpPost("Index_product")]
-		/*public IActionResult Index_product1([FromBody] string firstName, [FromBody] string lastName, [FromBody] List<int> masiv_product)
-		{
-			var Customer = _database.GetCustomer().FirstOrDefault(c => c.LastName == lastName
-				&& c.FirstName == firstName);
-
-			*//*var newOrder = new Order
-			{
-				CustomerId = Customer.CustomerId,
-				OrderDate = DateTime.Now,
-				TotalAmount = 1500, // Общая сумма заказа
-				OrderProducts = masiv_product.Select(productId => new OrderProduct { ProductId = productId }).ToList()
-
-			};*//*
-
-
-			//_database.AddOrder(newOrder);
-
-			//return Ok($"Received {masiv_product.Count} products for {firstName} {lastName}");
-			return Ok();
-		}*/
-
+		
+		//приймаєио дані для нового ордеру
 		[Route("Product/Index_product1")]
 		[HttpPost]
 		public IActionResult Index_product1([FromForm] string firstName, [FromForm] string lastName, [FromForm] List<int> masiv_product)
 		{
 			var customer = _database.GetCustomer().FirstOrDefault(c => c.LastName == lastName && c.FirstName == firstName);
 
-			if (customer == null)
-			{
-				return BadRequest("Customer not found");
-			}
-
 			var productPrices = _database.GetProduct() // Получаем список всех продуктов из базы данных
-	.Where(p => masiv_product.Contains(p.ProductId)) // Выбираем продукты, которые есть в списке masiv_product
-	.Sum(p => p.Price); // Суммируем их цены
+			.Where(p => masiv_product.Contains(p.ProductId)) // Выбираем продукты, которые есть в списке masiv_product
+			.Sum(p => p.Price); // Суммируем их цены
 
 			var newOrder = new Order
 			{
@@ -162,11 +148,7 @@ namespace magazun.Controllers
 			return Ok($"Order created for {firstName} {lastName} with {masiv_product.Count} products.");
 		}
 
-
-
-
-
-
+		//додаэмо новий ордер------------------------
 	}
 
 }
